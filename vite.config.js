03b4873c318128c2a0645e8eb566494a41775451
James Milner <jamesmilner22@hotmail.com>
2025-03-05 22:51:49 +0000

    use relative paths for assets

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
 export default defineConfig({
+  base: './',
   plugins: [preact()],
   build: {
     outDir: "docs"
